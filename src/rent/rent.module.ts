bc46d4d7772d296f6335fa7bded1cd9acc63a513
Evgeniya Akulenko <eakulenko@4xxi.com>
2022-10-03 14:34:12 +0300

    update deps

--- src/rent/rent.module.ts
+++ src/rent/rent.module.ts
@@ -3,10 +3,10 @@ import { RentController } from "./rent.controller";
 import { RentService } from "./rent.service";
 import { BLModule } from "src/bl/bl.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Auto, Book_session, Rate } from "../entities";
+import { Book_session, Rate } from "../entities";
 
 @Module({
-  imports: [BLModule, TypeOrmModule.forFeature([Auto, Rate, Book_session])],
+  imports: [BLModule, TypeOrmModule.forFeature([Rate, Book_session])],
   controllers: [RentController],
   providers: [RentService]
 })
--- src/rent/rent.service.ts
+++ src/rent/rent.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException, Logger, BadGatewayException } from "@nestjs/common";
+import { Injectable, BadRequestException, Logger } from "@nestjs/common";
 import { UtilsService } from "../bl/utils.service";
 import { CalcService } from "../bl/calc.service";
 import Const from "../constants";
@@ -6,8 +6,7 @@ import { IRate, RentInput, IRentOutput, IMonthlyReport } from "./rent.dto";
 import * as moment from "moment";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { Auto } from "../entities/auto.entity";
-import { Book_session, Rate } from "src/entities";
+import { Book_session, Rate } from "../entities";
 import { Brackets } from "typeorm";
 
 @Injectable()
@@ -16,7 +15,6 @@ export class RentService {
   constructor(
     private readonly utils: UtilsService,
     private readonly calc: CalcService,
-    @InjectRepository(Auto) private autoRepository: Repository<Auto>,
     @InjectRepository(Rate) private rateRepository: Repository<Rate>,
     @InjectRepository(Book_session) private bookSessionRepository: Repository<Book_session>
   ) {}
@@ -117,8 +115,10 @@ export class RentService {
              GREATEST(start_date, (date_trunc('MONTH', now()))::DATE))+1), \
         date_part('days',(date_trunc('month', NOW()) + interval '1 month - 1 day')) \
       FROM public.book_session \
-      WHERE extract(month FROM NOW()) = extract(month from start_date) OR \
-            extract(month FROM NOW()) = extract(month from end_date) \
+      WHERE (extract(month FROM NOW()) = extract(month from start_date) OR \
+            extract(month FROM NOW()) = extract(month from end_date)) AND \
+            (extract(year FROM NOW()) = extract(year from start_date) AND \
+             extract(year FROM NOW()) = extract(year from end_date)) \
       GROUP by auto_id \
     ) \
     SELECT auto_id, book_days, ROUND((book_days*100/days_in_month::int2), 0) AS \"book_percentage\", 'auto' AS \"key\" \
